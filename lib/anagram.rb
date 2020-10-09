a9b511575d7794bbc9d6aadf91ac4d7e90c79a86
Plimon Howard <plimon.howard@gmail.com>
2020-10-09 16:42:19 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(val)
+    awnswer =[]
+    val.each do |value|
+      if (value.split('').sort.join == word.split('').sort.join)
+        awnswer << value
+      end
+    end
+    return awnswer
+  end
+end
